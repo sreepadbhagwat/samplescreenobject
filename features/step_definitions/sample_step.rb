be41b58e89f90726e312ca27a83c15aef31148a9
KartikeyaJ <kartikeyaj@KartikeyaJs-MacBook-Air.local>
2016-08-23 12:51:29 -0400

    initial set up with screen object

--- features/step_definitions/sample_step.rb
+++ features/step_definitions/sample_step.rb
@@ -1,1 +1,8 @@
+When(/^I launch the app and verify button element$/) do
+  fail unless on(UICatalog).verify_uicatalog
+end
 
+
+Then(/^I should tap on the button$/) do
+  fail unless on(UICatalog).click_uicatalog
+end
